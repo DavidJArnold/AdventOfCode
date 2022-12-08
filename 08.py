ff237bb6ba8e1b6f0db8da984a127562198881c5
DavidJArnold <david.14587@gmail.com>
2022-12-08 22:33:06 +1100

    refactor 08

--- 08.py
+++ 08.py
@@ -18,36 +18,29 @@ for i in range(len(lines)):
 
 print("Part 1: ", count)
 
+
+def count_visible(trees, this_tree):
+    # search the list, seeing how far away is the first tree of equal/greater height?
+    x = 0
+    for tree in trees:
+        x += 1
+        if tree >= this_tree:
+            break
+    return x
+
+
 score = 0  # track maximum
 for i in range(1, len(lines) - 1):
     for j in range(1, len(lines) - 1):
         # iterate through each interior tree
         this_tree = lines[i][j]  # height of this tree
 
-        a = 0  # search above the tree, how far away is the first tree of equal/greater height?
-        # reverse the list since it is in order from top-to-bottom
-        for tree in reversed([lines[idx][j] for idx in range(i)]):
-            a += 1
-            if tree >= this_tree:
-                break
-
-        b = 0  # below
-        for tree in [lines[idx][j] for idx in range(i + 1, len(lines))]:
-            b += 1
-            if tree >= this_tree:
-                break
-
-        l = 0  # left (reversed list since we are looking "backwards" from this tree)
-        for tree in reversed(lines[i][:j]):
-            l += 1
-            if tree >= this_tree:
-                break
-
-        r = 0  # right
-        for tree in lines[i][j + 1 :]:
-            r += 1
-            if tree >= this_tree:
-                break
+        a = count_visible(reversed([lines[idx][j] for idx in range(i)]), this_tree)
+        b = count_visible(
+            [lines[idx][j] for idx in range(i + 1, len(lines))], this_tree
+        )
+        l = count_visible(reversed(lines[i][:j]), this_tree)
+        r = count_visible(lines[i][j + 1 :], this_tree)
 
         score = max(score, l * r * a * b)  # track best score
 
