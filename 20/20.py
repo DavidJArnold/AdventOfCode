55b4a1055f7185f967ad8b5e35e239de237c5614
DavidJArnold <david.14587@gmail.com>
2022-12-21 12:48:20 +1100

    day 20

--- 20/20.py
+++ 20/20.py
@@ -10,7 +10,7 @@ def part1(filename):
     # input is the raw input numbers
     input = get_input(filename)
     # output is a circular list holding indices into input
-    output = deque(list(range(len(input))))
+    output = deque(range(len(input)))
     for idx in range(len(input)):
         # loop over each element, moving the list underneath the element as we go
         # So if we have the list [1, 2, 3, 4, 5, 6] and want to move 3 forwards 2 spaces, we do
@@ -18,14 +18,12 @@ def part1(filename):
         # [2, 4, 5, 6, 1] -> [4, 5, 6, 1, 2]
         # then put the 3 back in where it  was [4, 5, 3, 6, 1, 2]
 
-        # find the element that we are looking at (by index not value)
-        num = input[idx]
         # find it's index in the output
         rem = output.index(idx)
         # remove it from the list
         output.remove(idx)
         # rotate the list backwards by the appropriate amount
-        output.rotate(-num)
+        output.rotate(-input[idx])
         # put the element back where it was
         output.insert(rem, idx)
     # convert list back to value of input rather than indices of input
@@ -47,17 +45,13 @@ def part2(filename):
     # and the shuffling is repeated 10 times
     input = get_input(filename)
     input = [i * 811589153 for i in input]
-    output = deque(list(range(len(input))))
-    total = 0
+    output = deque(range(len(input)))
     for _ in range(10):
         for idx in range(len(input)):
-            num = input[idx]
-
             rem = output.index(idx)
-            val = output[rem]
             output.remove(idx)
-            output.rotate(-num)
-            output.insert(rem, val)
+            output.rotate(-input[idx])
+            output.insert(rem, idx)
     final_list = [input[i] for i in output]
     first_zero = [i for i, v in enumerate(final_list) if v == 0][0]
 
