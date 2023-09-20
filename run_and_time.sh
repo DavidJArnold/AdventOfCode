0e205aed689f956ee8fad8ca202117a84451e966
DavidJArnold <david.14587@gmail.com>
2023-09-20 23:09:04 +1000

    Add rust debug mode timing

--- run_and_time.sh
+++ run_and_time.sh
@@ -7,7 +7,8 @@ function files_are_similar {
 }
 
 python_runtime=0
-rust_runtime=0
+rust_release_runtime=0
+rust_debug_runtime=0
 
 # Iterate through all subdirectories
 for dir in */; do
@@ -28,15 +29,24 @@ for dir in */; do
             start_time=$(date +%s.%N)
             rust_output=$("./target/release/day_$dir")
             end_time=$(date +%s.%N)
-            elapsed_time_rust=$(echo "$end_time - $start_time" | bc)
+            elapsed_time_rust_release=$(echo "$end_time - $start_time" | bc)
+
+            cargo build &>/dev/null
+            start_time=$(date +%s.%N)
+            rust_output=$("./target/debug/day_$dir")
+            end_time=$(date +%s.%N)
+            elapsed_time_rust_debug=$(echo "$end_time - $start_time" | bc)
+
             cd ..
 
             echo "Day $dir"
             if files_are_similar <(echo "$python_output") <(echo "$rust_output"); then
                 python_runtime=$(echo "$python_runtime + $elapsed_time_python" | bc)
-                rust_runtime=$(echo "$rust_runtime + $elapsed_time_rust" | bc)
+                rust_release_runtime=$(echo "$rust_release_runtime + $elapsed_time_rust_release" | bc)
+                rust_debug_runtime=$(echo "$rust_release_runtime + $elapsed_time_rust_debug" | bc)
                 echo "Python: ${elapsed_time_python}s"
-                echo "Rust:   ${elapsed_time_rust}s"
+                echo "Rust R:   ${elapsed_time_rust_release}s"
+                echo "Rust D:   ${elapsed_time_rust_debug}s"
             else
                 echo "Python and Rust outputs are different in '$dir' directory."
                 echo "$python_output"
@@ -47,10 +57,11 @@ for dir in */; do
 done
 
 echo "-----------------------------"
-echo "Python total runtime: ${python_runtime}s"
-echo "Rust total runtime: ${rust_runtime}s"
-saved=$(echo "100 * ($python_runtime - $rust_runtime) / $python_runtime" | bc -l)
+echo "Python total runtime:       ${python_runtime}s"
+echo "Rust debug total runtime:   ${rust_debug_runtime}s"
+echo "Rust release total runtime: ${rust_release_runtime}s"
+saved=$(echo "100 * ($python_runtime - $rust_release_runtime) / $python_runtime" | bc -l)
 rounded=$(printf "%.2f" "$saved")
-speedup=$(echo "$python_runtime / $rust_runtime" | bc -l)
+speedup=$(echo "$python_runtime / $rust_release_runtime" | bc -l)
 rounded_speedup=$(printf "%.2f" "$speedup")
 echo "Time saving: $rounded% (${rounded_speedup}x speed increase)"
