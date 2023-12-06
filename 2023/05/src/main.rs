a9cac5c6bf68db497b1e67793a8ab90d88ce6069
DavidJArnold <david.14587@gmail.com>
2023-12-06 23:06:08 +1100

    solve day 5 using inverse maps

--- 2023/05/src/main.rs
+++ 2023/05/src/main.rs
@@ -62,6 +62,14 @@ fn evaluate_seeds(seeds: Vec<u64>, maps: &Vec<Map>) -> u64 {
     min_val
 }
 
+fn evaluate_seed_inverse(seed: u64, maps: &Vec<Map>) -> u64 {
+    let mut val = seed;
+    for map in maps.iter().rev() {
+        val = evaluate_inverse_map(&map, val);
+    }
+    val
+}
+
 fn part1(filename: &str) -> u64 {
     let file_contents = std::fs::read_to_string(filename).unwrap();
     let input = file_contents.strip_suffix('\n').unwrap();
@@ -74,12 +82,12 @@ fn part2(filename: &str) -> u64 {
     let file_contents = std::fs::read_to_string(filename).unwrap();
     let input = file_contents.strip_suffix('\n').unwrap();
     let (seeds, maps) = parse_input(input);
-    
+
     let mut min_val: u64 = u64::max_value();
-    for seed_idx in 0..seeds.len()/2 {
+    for seed_idx in 0..seeds.len() / 2 {
         let mut seed_list: Vec<u64> = vec![];
-        let range_start = seeds[2*seed_idx];
-        let range_len = seeds[2*seed_idx +1];
+        let range_start = seeds[2 * seed_idx];
+        let range_len = seeds[2 * seed_idx + 1];
         for i in 0..range_len {
             seed_list.push(range_start + i);
         }
@@ -88,11 +96,38 @@ fn part2(filename: &str) -> u64 {
     min_val
 }
 
+fn part2_inverse(filename: &str) -> u64 {
+    let file_contents = std::fs::read_to_string(filename).unwrap();
+    let input = file_contents.strip_suffix('\n').unwrap();
+    let (seeds, maps) = parse_input(input);
+
+    let mut seed_list: Vec<(u64, u64)> = vec![];
+    for seed_idx in 0..seeds.len() / 2 {
+        let range_start = seeds[2 * seed_idx];
+        let range_len = seeds[2 * seed_idx + 1];
+        seed_list.push((range_start, range_start + range_len));
+    }
+
+    let mut test_val: u64 = 0;
+    let mut start = evaluate_seed_inverse(test_val, &maps);
+    while !seed_list
+        .iter()
+        .map(|x| x.0 <= start && x.1 >= start)
+        .reduce(|a, b| a || b)
+        .unwrap()
+    {
+        test_val += 1;
+        start = evaluate_seed_inverse(test_val, &maps);
+    }
+
+    test_val
+}
+
 fn main() {
     let filename = REAL_FILENAME;
     let ans1 = part1(filename);
     println!("Part 1: {}", ans1);
-    let ans2 = part2(filename);
+    let ans2 = part2_inverse(filename);
     println!("Part 2: {}", ans2);
 }
 
@@ -110,6 +145,12 @@ mod tests {
     #[test]
     fn test_part2() {
         let ans2 = part2(&TEST_FILENAME);
+        assert_eq!(ans2, 46)
+    }
+
+    #[test]
+    fn test_part2_inverse() {
+        let ans2 = part2_inverse(&TEST_FILENAME);
         assert_eq!(ans2, 46)
     }
 
@@ -132,5 +173,27 @@ mod tests {
         assert_eq!(evaluate_map(&map, 99), 51);
         assert_eq!(evaluate_map(&map, 97), 99);
         assert_eq!(evaluate_map(&map, 53), 55);
+    }
+
+    #[test]
+    fn test_inverse_maps() {
+        let map_section_1: MapSection = MapSection {
+            destination_start: 50,
+            source_start: 98,
+            length: 2,
+        };
+        let map_section_2: MapSection = MapSection {
+            destination_start: 52,
+            source_start: 50,
+            length: 48,
+        };
+        let map = Map {
+            sections: vec![map_section_1, map_section_2],
+        };
+        assert_eq!(evaluate_inverse_map(&map, 50), 98);
+        assert_eq!(evaluate_inverse_map(&map, 51), 99);
+        assert_eq!(evaluate_inverse_map(&map, 99), 97);
+        assert_eq!(evaluate_inverse_map(&map, 55), 53);
+        assert_eq!(evaluate_inverse_map(&map, 81), 79);
     }
 }
